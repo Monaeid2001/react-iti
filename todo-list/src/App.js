5d972b1aca536122b5005492ce8ccce1c94ff7c1
Mona Eid <104376815+Monaeid2001@users.noreply.github.com>
2024-03-18 14:23:30 +0200

    Add files via upload

---
+++ todo-list/src/App.js
@@ -0,0 +1,54 @@
+import React, { useRef, useState } from "react";
+import "./App.css";
+import Button from "./Button";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef();
+
+  const handleAddTodo = () => {
+    const text = inputRef.current.value;
+    const newItem = { completed: false, text };
+    setTodos([...todos, newItem]);
+    inputRef.current.value = "";
+  };
+
+  const handleItemDone = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
+  const handleDeleteItem = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="App">
+      <h2>To Do List</h2>
+      <div className="to-do-container">
+        <ul>
+          {todos.map(({ text, completed }, index) => (
+            <div className="item" key={index}>
+              <li
+                className={completed ? "done" : ""}
+                onClick={() => handleItemDone(index)}
+              >
+                {text}
+              </li>
+              <span onClick={() => handleDeleteItem(index)} className="trash">
+                ❌
+              </span>
+            </div>
+          ))}
+        </ul>
+        <input ref={inputRef} placeholder="Enter item..." />
+        <Button onClick={handleAddTodo}>Add</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
